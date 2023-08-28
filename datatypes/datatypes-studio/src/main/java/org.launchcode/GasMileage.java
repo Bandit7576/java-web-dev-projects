0946c545dc3a48ecfc38e62075604f1dad0811f1
unknown <wilberhasty75@gmail.com>
2023-08-28 06:14:38 -0500

    Ch 2 exercises part C

--- datatypes/datatypes-studio/src/main/java/org.launchcode/GasMileage.java
+++ datatypes/datatypes-studio/src/main/java/org.launchcode/GasMileage.java
@@ -1,2 +1,17 @@
-package org.launchcode;public class GasMileage {
+package org.launchcode;
+
+import java.util.Scanner;
+
+public class GasMileage {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of miles traveled: ");
+        int miles = input.nextInt();
+        System.out.println("Enter the number of gallons of fuel used: ");
+        int gallons = input.nextInt();
+
+        int mileage = miles / gallons;
+
+        System.out.println("You are getting " + mileage + " mpg!");
+    }
 }
