78ac96e52a57ce3c9e51dc985bcf361a9f202f84
unknown <wilberhasty75@gmail.com>
2023-08-31 10:43:52 -0500

    ch 2 exercises part C

--- classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Main.java
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        MenuItem newMenu = new MenuItem(5.29, "bigMac","");
     }
 }
---
+++ datatypes/datatypes-exercises/src/main/java/org/launchcode/milesPerGallon.java
@@ -0,0 +1,16 @@
+package org.launchcode;
+
+import java.util.Scanner;
+
+public class milesPerGallon {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter number of miles traveled: ");
+        Double miles = input.nextDouble();
+        System.out.println("Enter number of gallons used: ");
+        Double gallons = input.nextDouble();
+        double  mpg = miles / gallons;
+
+        System.out.println("You are getting " + mpg + " miles per gallon!");
+    }
+}
